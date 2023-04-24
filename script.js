a9f3521fc7a72245a9772f642cd4d82cebe46395
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-24 13:53:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let name=navigator.appName;
+let version=navigator.appVersion;
+let info=document.getElementById("browser-info");
+info.innerText="You are using " + name + " version " + version;
